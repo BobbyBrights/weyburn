e0032feeeaccee51ac020fddae4692c78e970144
niniko1997 <m.nino97@gmail.com>
2017-10-18 21:49:49 -0700

    started data component

---
+++ js/script.js
@@ -0,0 +1,28 @@
+d3.json('rainfall.json', function(rainfallData) {
+
+
+/* Label data */
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'];
+var years = [];
+for(var i = 1980; i < 2011; i++)
+    i % 10 === 0 ? years.push(i) : years.push('');
+
+/* Create the chart */
+var chart = circularHeatChart()
+    .segmentHeight(5)
+    .innerRadius(20)
+    .numSegments(12)
+    .domain([50, 200])
+    .range(['white', 'blue'])
+    .segmentLabels(months)
+    .radialLabels(years);
+
+d3.select('#chart')
+    .selectAll('svg')
+    .data([rainfallData])
+    .enter()
+    .append('svg')
+    .call(chart);
+
+});
